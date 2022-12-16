03c3a61a3a5ba4e4e25530d3ccfba707a3332741
Wojciech Panfil <wojtek.panfil@hotmail.com>
2022-12-16 17:35:03 +0100

    Add removal tests
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

--- validator.py
+++ validator.py
@@ -9,10 +9,16 @@ class Validator:
 
     def add_x_records(self, x):
         numbers = [i for i in range(x)]
+        numbers_removal = [i for i in range(x)]
         with open(self._filename, "w+") as f:
             while len(numbers):
                 number = random.choice(numbers)
                 numbers.remove(number)
                 # f.write(f"INSERT {number} {random.random()} {random.random()} {random.random()}")
                 f.write(f"INSERT {number}\n")
+                f.write(f"PRINT\n")
+            while len(numbers_removal):
+                number = random.choice(numbers_removal)
+                numbers_removal.remove(number)
+                f.write(f"DELETE {number}\n")
                 f.write(f"PRINT\n")
